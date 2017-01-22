83a1080ec6027574c3ce1eea4d4545bea1529fa9
X D <mail@danielmoeck.de>
2017-01-22 04:14:19 +0100

    deleted some  audio files  :Bowtie:

--- app/media/js/sound.js
+++ app/media/js/sound.js
@@ -1,7 +1,7 @@
 function Sound() {
 	//gamesound wird hier geladen
 	var audio = new Audio('media/Gamesound64kbs.mp3');
-	var waveshot = new Audio('media/waveexplosion3.wav');
+	var waveshot = new Audio('media/waveexplosion3.wav'); //evtl in mp3 konvertieren
 
 	function playGameSong() {
 		audio.play();
